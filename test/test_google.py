48b98f807674bb6849ef92d22753c3f332111799
mikhailnekhaenok <nekhaenok@gmail.com>
2025-05-24 13:10:08 -0700

    simplify comparison logic, remove unused requirements

--- test/test_google.py
+++ test/test_google.py
@@ -7,17 +7,10 @@ class TestGoogleFinance:
         assert app.find_element(app.gf._header)
 
     def test_compare_current_and_expected_stocks(self, app):
-        expected_stock_symbols = ["NFLX", "MSFT", "TSLA", "GOOGL", "AAPL", "AMZN"]
-        current_stock_list = app.gf.get_you_may_be_interested_list()
+        expected_stock_symbols = set(["NFLX", "MSFT", "TSLA", "GOOG", "AAPL", "AMZN"])
+        current_stock_list = set(app.gf.get_you_may_be_interested_list())
 
-        unexpected_stock = []
-        for stock in current_stock_list:
-            if stock not in expected_stock_symbols:
-                unexpected_stock.append(stock)
+        unexpected_stock = current_stock_list - expected_stock_symbols
+        missing_expected_stock = expected_stock_symbols - current_stock_list
 
-        expected_stock = []
-        for stock in expected_stock_symbols:
-            if stock not in current_stock_list:
-                expected_stock.append(stock)
-
-        print(f"Unexpected stocks in the list {unexpected_stock}, Expected stocks that are not in the list {expected_stock}")
+        print(f"Unexpected stocks in the list {unexpected_stock}, Expected stocks that are not in the list {missing_expected_stock}")
